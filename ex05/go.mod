bc03af3e199a1953bb689110d7b654ee8acb6e65
Carlos <carlos.limalopes@outlook.com>
2024-06-09 19:59:02 -0300

    ex05 - complete

--- ex05/go.mod
+++ ex05/go.mod
@@ -1,3 +1,5 @@
 module github.com/KZLopes/gophercises/mapBuilder
 
 go 1.22.0
+
+require golang.org/x/net v0.26.0
--- ex05/main.go
+++ ex05/main.go
@@ -1,20 +1,135 @@
 package main
 
 import (
+	"encoding/xml"
+	"flag"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
+	"strings"
 )
 
-func main() {
-	url := "https://www.calhoun.io"
+const xmlns = "htttp://www.sitemaps.org/schemas/sitemap/0.9"
 
-	resp, err := http.Get(url)
+type loc struct {
+	Value string `xml:"loc"`
+}
+
+type urlset struct {
+	Urls  []loc  `xml:"url"`
+	Xmlns string `xml:"xlmns,attr"`
+}
+
+func main() {
+	urlFlag := flag.String("url", "https://gophercises.com", "the url to create a sitemap for")
+	maxDepth := flag.Int("depth", 3, "the maximum deep for each page")
+	flag.Parse()
+
+	pages := bfs(*urlFlag, *maxDepth)
+
+	toXml := urlset{
+		Urls:  make([]loc, len(pages)),
+		Xmlns: xmlns,
+	}
+
+	for i, page := range pages {
+		toXml.Urls[i] = loc{page}
+	}
+
+	fmt.Print(xml.Header)
+	enc := xml.NewEncoder(os.Stdout)
+	enc.Indent("", "   ")
+	if err := enc.Encode(toXml); err != nil {
+		panic(err)
+	}
+
+}
+
+func bfs(urlStr string, maxDepth int) []string {
+	seen := make(map[string]struct{})
+	var q map[string]struct{}
+	nextQ := map[string]struct{}{
+		urlStr: {},
+	}
+	for i := 0; i <= maxDepth; i++ {
+		q, nextQ = nextQ, make(map[string]struct{})
+		if len(q) == 0 {
+			break
+		}
+		for url := range q {
+			if _, ok := seen[url]; ok {
+				continue
+			}
+			seen[url] = struct{}{}
+			for _, link := range get(url) {
+				if _, ok := seen[link]; !ok {
+					nextQ[link] = struct{}{}
+				}
+			}
+		}
+	}
+
+	ret := make([]string, 0, len(seen))
+	for url := range seen {
+		ret = append(ret, url)
+	}
+	return ret
+}
+
+func get(urlStr string) []string {
+	resp, err := http.Get(urlStr)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer resp.Body.Close()
 
-	io.Copy(os.Stdout, resp.Body)
+	reqUrl := resp.Request.URL
+	baseUrl := &url.URL{
+		Scheme: reqUrl.Scheme,
+		Host:   reqUrl.Host,
+	}
+	base := baseUrl.String()
+
+	return filter(hrefs(resp.Body, base), withPrefix(base))
 }
+
+func hrefs(r io.Reader, base string) []string {
+	var ret []string
+	links, _ := Parse(r)
+	for _, l := range links {
+		switch {
+		case strings.HasPrefix(l.Href, "/"):
+			ret = append(ret, base+l.Href)
+		case strings.HasPrefix(l.Href, "http"):
+			ret = append(ret, l.Href)
+		}
+	}
+
+	return ret
+}
+
+func filter(links []string, keepFn func(string) bool) []string {
+	var ret []string
+	for _, link := range links {
+		if keepFn(link) {
+			ret = append(ret, link)
+		}
+	}
+	return ret
+}
+
+func withPrefix(pfx string) func(string) bool {
+	return func(link string) bool {
+		return strings.HasPrefix(link, pfx)
+	}
+}
+
+// GET Page DONE!
+// Parse All Links DONE!
+// Build Proper Url with the Links
+// Filter Out Lins with Different Domain
+// Find all the Pages (BFS)
+// Print Out XML
